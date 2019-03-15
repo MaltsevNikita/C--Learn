0d1ce29e91d7ca97536f9742a5a6170dc8491448
Maltsev Nikita <mn121288@gmail.com>
2019-03-15 08:26:27 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Array1 = new int[,] { { 1, 2, 3, 4, 5 }, { 5, 3, 7, 1, 5 } };
+            public void  GetElement(int row, int column, int [,] a)
+            {
+                for(int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        if (i == row && j == column)
+                        {
+                            Console.WriteLine( a[row, column] );
+                        }
+                    }
+                }
+                
+             
+            Console.ReadLine();
+
+            }
+    }
+}
