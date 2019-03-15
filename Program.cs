ef46f934e043efea7955820cab9e0d80c1f1d518
Maltsev Nikita <mn121288@gmail.com>
2019-03-15 18:42:09 +0400

    Сделал вторую часть задания

--- Program.cs
+++ Program.cs
@@ -10,13 +10,13 @@ namespace Theme1
     {
         static int[,] Array = new int[,] { { 1, 2, 3, 4, 5 }, { 5, 3, 7, 1, 5 } };
 
-        static void GetElement(int row, int column, int[,] a)
+        static void getElement(int row, int column, int[,] arr)
         {
-            for (int i = 0; i < Array.GetLength(0); i++)
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
-                for (int j = 0; j < Array.GetLength(1); j++)
+                for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                        Console.Write(a[i, j]);
+                        Console.Write(arr[i, j]);
                 
                 }
                 Console.WriteLine();
@@ -26,9 +26,19 @@ namespace Theme1
 
         }
 
+        static void getElement2(int row, int column, int [,] arr)
+        {
+            arr[0,0] = arr[row, column];
+            Console.WriteLine(arr[0, 0]);
+        }
+
         static void Main(string[] args)
         {
-            GetElement(0, 1, Array);
+            getElement(0, 1, Array);
+            Console.WriteLine('\n');
+            Console.WriteLine('\n');
+            Console.WriteLine('\n');
+            getElement2(1,1,Array);
             Console.ReadLine();
         }
     }
