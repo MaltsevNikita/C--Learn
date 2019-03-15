df75256238356ebb0360c16798230066d31334c5
Maltsev Nikita <mn121288@gmail.com>
2019-03-15 12:12:26 +0400

    Third commit

--- Program.cs
+++ Program.cs
@@ -8,23 +8,24 @@ namespace Theme1
 {
     class Program
     {
+        int[,] imyArray2 = new int[,] { { 1, 2, 3, 4, 5 }, { 5, 3, 7, 1, 5 } };
+
         static void Main(string[] args)
         {
-            int[,] Array1 =  { { 1, 2, 3, 4, 5 }, { 5, 3, 7, 1, 5 } };
-            public void  GetElement(int row, int column, int [,] a)
+            public void GetElement(int row, int column, int[,] a)
             {
-                for(int i = 0; i < row; i++)
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
                 {
-                    for (int j = 0; j < column; j++)
+                    if (i == row && j == column)
                     {
-                        if (i == row && j == column)
-                        {
-                            Console.WriteLine( a[row, column] );
-                        }
+                        Console.WriteLine(a[row, column]);
                     }
                 }
-                
-             
+            }
+
+
             Console.ReadLine();
 
             }
