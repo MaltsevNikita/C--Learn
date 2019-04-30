700eb07573354e94175d35f0638725f5b19f47ae
Maltsev Nikita <mn121288@gmail.com>
2019-04-30 18:05:55 +0400

    Доделал Linq

--- Program.cs
+++ Program.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 
 namespace Theme1
 {
+    public struct ValueWithIndex
+    {
+        public int Item, X, Y;
+
+        public ValueWithIndex(int item, int x, int y)
+        {
+            Item = item;
+            X = x;
+            Y = y;
+        }
+    }
+
     class Program
     {
-        static int[,] Array = new int[,] { { 1, 2, 3, 4, 5 }, 
+        static int[,] Array = new int[,] { { 1, 2, 3, 4, 5 },
                                            { 5, 3, 7, 1, 5 } };
 
         static void getElement1(int row, int column, int[,] arr)
@@ -17,33 +29,48 @@ namespace Theme1
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                        Console.Write(arr[i, j]);
-                
+                    Console.Write(arr[i, j]);
+
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("Значение, возвращеннное методом getElement1:");
-            Console.WriteLine(Array[ row,column ]);
+            Console.WriteLine(Array[row, column]);
 
         }
 
-        static void getElement2(int row, int column, int [,] arr)
+        static void getElement2(int row, int column, int[,] arr)
         {
-            arr[0,0] = arr[row, column];
+            arr[0, 0] = arr[row, column];
             Console.WriteLine("Значение первого параметра после выполнения метода getElement2:");
             Console.WriteLine(arr[0, 0]);
         }
 
-        static int[,] Array2= new int[,] { { 1, 2, 3, 4, 5 },
-                                           { 5, 3, 7, 1, 5 } };
+        static int[,] A2 = new int[,] { { 1, 2, 3, 4, 5 },
+                                       { 10, 2, 30, 40, 50} };
 
-        static void getElement3(int FoundRow, int FoundColumn, int[,] arr, int element)
+        public static IEnumerable<ValueWithIndex> Flatten(int[,] map)
         {
-            var found =
-                from num in Array2
-                where (num == element)
-                select num;   
-            //пишем код
+            for (int row = 0; row < map.GetLength(0); row++)
+            {
+                for (int col = 0; col < map.GetLength(1); col++)
+                {
+                    var item = map[row, col];
+                    ValueWithIndex stct = new ValueWithIndex(item, row, col);
+                    yield return stct;
+                }
+            }
+        }
+            static void getElement3(int x, int y, int[,] array, int value)
+        {
+            var item = Flatten(array).Where(val => val.Item == value).FirstOrDefault();
+            x = item.X;
+            y = item.Y;
+            Console.WriteLine(item.Item);
+            Console.WriteLine("индексы найденного элемента:");
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            
         }
         static void Main(string[] args)
         {
@@ -51,7 +78,13 @@ namespace Theme1
             Console.WriteLine('\n');
             Console.WriteLine('\n');
             Console.WriteLine('\n');
-            getElement2(1,1,Array);
+            getElement2(1, 1, Array);
+            Console.WriteLine('\n');
+            Console.WriteLine('\n');
+            Console.WriteLine('\n');
+            Console.WriteLine("Значение возвращаемое методом getElement3:");
+            getElement3(1, 1, A2, 2);
+
             Console.ReadLine();
         }
     }
